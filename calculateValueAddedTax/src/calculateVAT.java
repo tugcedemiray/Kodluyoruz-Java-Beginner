982064ed5beb5acbffcd08224fd8146c1f6e197f
Tugce Demiray <demiraytugce1@gmail.com>
2022-06-18 18:18:35 +0300

    calculate vat

---
+++ calculateValueAddedTax/src/calculateVAT.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class calculateVAT {
+    public static void main(String[] args) {
+        double amountOfMoney, percentageOfVAT, amountOfVAT, moneyWithVAT;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the amount of money : ");
+        amountOfMoney = input.nextDouble();
+        percentageOfVAT = 0 < amountOfMoney && amountOfMoney <= 1000 ? 0.18 : 0.08;
+        amountOfVAT = amountOfMoney * percentageOfVAT;
+        moneyWithVAT = amountOfMoney + amountOfVAT;
+        System.out.println("Without VAT : " + amountOfMoney);
+        System.out.println("Percentage of VAT : " + percentageOfVAT);
+        System.out.println("Amount of VAT : " + amountOfVAT);
+        System.out.println("With VAT : " + moneyWithVAT);
+    }
+}
