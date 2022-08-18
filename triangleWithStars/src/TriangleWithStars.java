bf7247fa86a5b2f17941939f870cb6fa38067e4b
Tugce Demiray <demiraytugce1@gmail.com>
2022-08-18 22:55:27 +0300

    triangle and diamond with stars

--- triangleWithStars/src/TriangleWithStars.java
+++ triangleWithStars/src/TriangleWithStars.java
@@ -7,7 +7,7 @@ public class TriangleWithStars {
         System.out.print("Enter a number : ");
         number = input.nextInt();
         for (int i = 1; i <= number ; i++) {
-            for (int j = 0; j <= (number - i) ; j++) {
+            for (int j = 1; j <= (number - i) ; j++) {
                 System.out.print(" ");
             }
             for (int k = 1; k <= (2*i - 1) ; k++) {
---
+++ triangleWithStars/src/TriangleWithStarsWhileExample.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class TriangleWithStarsWhileExample {
+    public static void main(String[] args) {
+        int number, height, space, stars;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        number = input.nextInt();
+
+        height = 1;
+        while (height <= number) {
+            //System.out.println(height);
+            space = 1;
+            while (space <= (number - height)) {
+                System.out.print(" ");
+                space++;
+            }
+            stars = 1;
+            while (stars <= (2 * height - 1)) {
+                System.out.print("*");
+                stars++;
+            }
+            height++;
+            System.out.println();
+        }
+
+    }
+}
